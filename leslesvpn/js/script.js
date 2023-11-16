83e713a066aeb7beb4c4d34e905bd120e2605145
Caio Sales <developcaio@gmail.com>
2023-11-16 17:31:52 -0300

    arquivos

--- leslesvpn/js/script.js
+++ leslesvpn/js/script.js
@@ -6,3 +6,30 @@ function menuShow() {
         menuMobile.classList.add('open')
     }
 }
+
+$('.owl-carousel').owlCarousel({
+    stagePadding:50,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+            stagePadding:100,
+        },
+        500:{
+            stagePadding:2,
+            loop:false,
+        },
+        650:{
+            loop:false,
+            stagePadding:50,
+        },
+        1000:{
+            items:3,
+            loop:false,
+            nav:false,
+            stagePadding:200,
+        }
+    }
+})
