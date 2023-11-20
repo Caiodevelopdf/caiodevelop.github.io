294aadd2fb051db7ad2a2dec13a72b2f25b26030
Caio Sales <developcaio@gmail.com>
2023-11-20 18:27:22 -0300

    atualizaçoes e novos arquivos

--- leslesvpn/js/script.js
+++ leslesvpn/js/script.js
@@ -7,29 +7,68 @@ function menuShow() {
     }
 }
 
-$('.owl-carousel').owlCarousel({
-    stagePadding:50,
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1,
-            stagePadding:100,
-        },
-        500:{
-            stagePadding:2,
-            loop:false,
-        },
-        650:{
-            loop:false,
-            stagePadding:50,
-        },
-        1000:{
-            items:3,
-            loop:false,
-            nav:false,
-            stagePadding:200,
+$(document).ready(function(){
+    var settings1 = {
+        stagePadding:50,
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1,
+                stagePadding:100,
+            },
+            500:{
+                stagePadding:2,
+                loop:false,
+            },
+            600:{
+                items:3,
+                loop:false,
+                stagePadding:100,
+            },
+            943:{
+                items:3,
+                loop:false,
+                stagePadding:200,
+            },
+            
+            1157:{
+                items:3,
+                loop:false,
+                nav:false,
+                stagePadding:50,
+            }
+
         }
-    }
-})
+
+    };
+    var  settings2 = {
+        stagePadding:50,
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1,
+                stagePadding:50,
+            },
+            500:{
+                stagePadding:2,
+                loop:false,
+            },
+            650:{
+                loop:true,
+                stagePadding:0,
+            },
+            1375:{
+                items:3,
+                loop:false,
+                nav:false,
+                stagePadding:50,
+            }
+        }
+    };
+    $('#carousel1').owlCarousel(settings1);
+    $('#carousel2').owlCarousel(settings2);
+});
